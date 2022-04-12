2475db4923de298dcde0209933c3551d20ee7002
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-04-12 15:00:14 -0500

    String_functions

--- simple_shell.h
+++ simple_shell.h
@@ -4,4 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* String functions */
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(const char *s1, const char *s2);
+char *_strdup(char *str);
+
+
 #endif
